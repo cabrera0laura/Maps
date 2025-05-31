705fb65ea3d1d72684b791a5b98085400536f8a8
cabrera0laura <lucas56hoseok@gmail.com>
2025-05-31 10:04:01 -0300

    Utilizando serviço de Geolocalização do Google

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maps
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
